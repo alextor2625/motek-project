9c8514df23382eca0cfe6572404cfe6357964a50
alextor26 <alextor2625@gmail.com>
2023-09-30 20:24:59 -0400

    Content diplays on profile page, can edit profile.

--- routes/users.js
+++ routes/users.js
@@ -1,11 +1,71 @@
 var express = require('express');
 var router = express.Router();
 
-const {isLoggedIn, isLoggedOut } = require('../middleware/route-guard')
+const { isLoggedIn, isLoggedOut } = require('../middleware/route-guard')
+
+const User = require('../models/User')
 
 /* GET users listing. */
 router.get('/profile', isLoggedIn, (req, res, next) => {
-  res.render('users/user-profile',req.session.user);
+  res.render('users/user-profile', req.session.user);
 });
+
+router.get('/profile/edit', (req, res, next) => {
+  const { fullname, email } = req.session.user;
+  let name = fullname.split(' ');
+  res.render('users/edit-user-profile', { firstname: name[0], lastname: name[1], email });
+})
+
+router.post('/profile/edit', (req, res, next) => {
+  const { fullname, username } = req.session.user;
+  const { firstname, lastname, email } = req.body;
+
+  User.findOne({ email })
+    .then(foundUserEmail => {
+      if (foundUserEmail) {
+        if (username === foundUserEmail.username) {
+          if ((firstname + ' ' + lastname) !== fullname) {
+            User.findOneAndUpdate({ username }, { fullname: firstname + ' ' + lastname }, { new: true })
+              .then(updatedUser => {
+                console.log('User Updated ===> ', updatedUser);
+                req.session.user = updatedUser
+                return res.render('users/user-profile', req.session.user)
+              })
+              .catch(err => {
+                console.log(err);
+                next(err);
+              })
+          } else {
+            console.log("No Change on User.");
+            return res.render('users/user-profile', req.session.user)
+          }
+        } else {
+          console.log("LINE 43 USERS.JS");
+          res.render('users/edit-user-profile', { firstname, 
+                                                  lastname, 
+                                                  email, 
+                                                  errorMessage: "Email already in use." })
+        }
+      } else if (!((firstname + ' ' + lastname) == fullname) || !(email === req.session.user.email)) {
+          User.findOneAndUpdate({ username }, { fullname: firstname + ' ' + lastname, email }, { new: true })
+            .then(updatedUser => {
+              console.log('User Updated ===> ', updatedUser);
+              req.session.user = updatedUser
+              return res.render('users/user-profile', req.session.user)
+            })
+            .catch(err => {
+              console.log(err);
+              next(err);
+            })
+      }
+    })
+    .catch(err => {
+      console.log(err);
+      next(err);
+    })
+
+
+
+})
 
 module.exports = router;
