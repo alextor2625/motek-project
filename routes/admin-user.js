f4dc7e3d9df561ffc99ded89f5f3836be0b8ed14
alextor26 <alextor2625@gmail.com>
2023-10-01 22:29:57 -0400

    made some routes for admin

--- routes/admin-user.js
+++ routes/admin-user.js
@@ -6,10 +6,25 @@ const User = require('../models/User')
 
 
 /* GET home page. */
-router.get('/user', function (req, res, next) {
+router.get('/user', isLoggedIn, function (req, res, next) {
     console.log("Admin User recognised");
     const isLoggedIn = req.session.user ? true : false;
     res.render('users/admin-profile', { isLoggedIn, admin: true });
 });
+
+
+router.get('/user/lunchmenu/edit', isLoggedIn, (req,res,next) =>{
+    const isLoggedIn = req.session.user ? true : false;
+    res.render('menus/edit-lunch-menu', { isLoggedIn, admin: true })
+})
+router.get('/user/dinnermenu/edit', isLoggedIn, (req,res,next) =>{
+    const isLoggedIn = req.session.user ? true : false;
+    res.render('menus/edit-dinner-menu', { isLoggedIn, admin: true })
+})
+router.get('/user/rewards/edit', isLoggedIn, (req,res,next) =>{
+    const isLoggedIn = req.session.user ? true : false;
+    res.render('rewards/edit-rewards', { isLoggedIn, admin: true })
+})
+
 
 module.exports = router;
