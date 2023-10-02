a00a2f49817cd87e77cdb8ab60a4f82c0095e10d
alextor26 <alextor2625@gmail.com>
2023-10-01 22:54:08 -0400

    added menu model

---
+++ models/Menu.js
@@ -0,0 +1,35 @@
+const { Schema, model } = require('mongoose');
+
+const Menu = new Schema({
+    menuType: {
+        type: String,
+        enum:['Lunch','Dinner'],
+        required: true,
+        trim: true
+    },
+    itemName:{
+        type: String,
+        required: true,
+        trim: true,
+        unique: true
+    },
+    category:{
+        type: String,
+        required: true,
+        trim: true,
+    },
+    description:{
+        type: String,
+        required: true
+    },
+    calories:{
+        type: Number,
+        required: true,
+        default: 0
+    }
+},
+{
+  timestamps: true
+});
+
+module.exports = model('Menu', Menu);
